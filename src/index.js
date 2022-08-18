6d6bb0e090fbe170fa0c10b477aa4c9dc12c8a9e
atilla kalay <atilla.kalayy@gmail.com>
2022-08-18 17:00:43 +0300

    Fourth lesson complated.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Post from './components/Post/Post';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Post/>
   </React.StrictMode>
 );
 
